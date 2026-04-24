6b824cc0efd364c24193a920e133f63468528c38
Saad <saadzulfiqar7777@gmail.com>
2026-04-24 20:07:50 +0500

    feat: refactored route handlers

--- server.js
+++ server.js
@@ -2,50 +2,11 @@ const http = require("node:http");
 
 const routes = {
   // Route: GET / - main home page
-  "GET /": (req, res) => {
-    res.writeHead(200, "{'content-type' = 'text/plain'}");
-    res.end("Hello From Server!");
-  },
+  "GET /": homeHandler,
   // Route: POST /data - Handles incoming JSON payloads
-  "POST /": (req, res) => {
-    let chunks = [];
-
-    // Listen for incoming data stream 'chunks'
-    req.on("data", (chunk) => {
-      chunks.push(chunk);
-    });
-
-    // Event fired when the entire request body has been received
-    req.on("end", () => {
-      try {
-        // Combine chunks and convert them into a readable string
-        const body = Buffer.concat(chunks).toString();
-
-        // Check if the body is empty before attempting to parse
-        if (!body || body.trim() === "") {
-          res.writeHead(400, { "Content-Type": "text/plain" });
-          return res.end("There is nothing in request");
-        }
-
-        // Parse string into a JavaScript object
-        const parsed = JSON.parse(body);
-
-        // Success response: Echo back the data with a new status message
-        res.writeHead(200, { "Content-Type": "application/json" });
-        parsed.message = "Data received";
-        res.end(JSON.stringify(parsed));
-      } catch (err) {
-        // Handle bad JSON
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        res.end("Wrong JSON Format");
-      }
-    });
-  },
+  "POST /": postHandler,
   // Route: GET /api - Returns a JSON object
-  "GET /api": (req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ status: "good", message: "Hello From Server" }));
-  },
+  "GET /api": apiHandler,
 };
 
 // Initialize the server instance
@@ -54,8 +15,8 @@ const server = http.createServer((req, res) => {
   const handler = routes[key];
 
   if (handler) {
-    handler (req, res);
-  }else{
+    handler(req, res);
+  } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("There is nothing here");
   }
@@ -65,3 +26,49 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+//route handler function implementations
+function homeHandler(req, res) {
+  res.writeHead(200, "{'content-type' = 'text/plain'}");
+  res.end("Hello From Server!");
+}
+
+function postHandler(req, res) {
+  let chunks = [];
+
+  // Listen for incoming data stream 'chunks'
+  req.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+
+  // Event fired when the entire request body has been received
+  req.on("end", () => {
+    try {
+      // Combine chunks and convert them into a readable string
+      const body = Buffer.concat(chunks).toString();
+
+      // Check if the body is empty before attempting to parse
+      if (!body || body.trim() === "") {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        return res.end("There is nothing in request");
+      }
+
+      // Parse string into a JavaScript object
+      const parsed = JSON.parse(body);
+
+      // Success response: Echo back the data with a new status message
+      res.writeHead(200, { "Content-Type": "application/json" });
+      parsed.message = "Data received";
+      res.end(JSON.stringify(parsed));
+    } catch (err) {
+      // Handle bad JSON
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end("Wrong JSON Format");
+    }
+  });
+}
+
+function apiHandler(req, res) {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ status: "good", message: "Hello From Server" }));
+}
